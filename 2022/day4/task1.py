aa5c234522a8ba053620c4d36e2a9af003a5922a
LizaGenke <lizagenke@gmail.com>
2022-12-04 11:31:41 +0100

    day 4 puzzle solved

---
+++ 2022/day4/task1.py
@@ -0,0 +1,24 @@
+input = "day4\input.txt"
+
+overlapping_pairs_count = 0
+
+with open(input) as f:
+    while True:
+        line = f.readline()
+        if not line:
+            break   
+        ranges = line.split(",")
+        first_range = ranges[0].split("-")
+        first_range_numbers = []
+        for number in first_range:
+            first_range_numbers.append(int(number))
+        second_range = ranges[1].split("-")
+        second_range_numbers = []
+        for number in second_range:
+            second_range_numbers.append(int(number))
+        if (((first_range_numbers[0] <= second_range_numbers[0]) and (first_range_numbers[1]>=second_range_numbers[1])) or 
+            ((first_range_numbers[0] >= second_range_numbers[0]) and (first_range_numbers[1]<=second_range_numbers[1]))):
+            overlapping_pairs_count += 1
+            print(first_range, " and ", second_range)
+
+print(overlapping_pairs_count)
